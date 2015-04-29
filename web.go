939338c82b1ba29ef0f01ff42946a14db5e85bad
duwei <duv123@gmail.com>
2015-04-29 19:44:21 +0800

    test

--- web.go
+++ web.go
@@ -1,20 +1,52 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"net/http"
+	"github.com/hidu/pproxy/serve"
+	"log"
 	"os"
 )
 
-func main() {
-	http.HandleFunc("/", hello)
-	fmt.Println("listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-	if err != nil {
-		panic(err)
+var configPath = flag.String("conf", "./conf/pproxy.conf", "pproxy's config file")
+var port = flag.Int("port", 0, "proxy port")
+var vv = flag.Bool("vv", false, "debug,log request with more detail")
+var show_conf = flag.Bool("demo_conf", false, "show default conf")
+
+var version = flag.Bool("v", false, "show version")
+
+
+func init() {
+	df := flag.Usage
+
+	flag.Usage = func() {
+		df()
+		fmt.Fprintln(os.Stderr, "\n HTTP protocol analysis tool\n https://github.com/hidu/pproxy/\n")
 	}
 }
 
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(res, "hello, heroku")
+func main() {
+	flag.Parse()
+
+	if *show_conf {
+		demo_conf := serve.GetDemoConf()
+		fmt.Println(demo_conf)
+		os.Exit(0)
+	}
+
+	if *version {
+		fmt.Println("pproxy version:", serve.GetVersion())
+		os.Exit(0)
+	}
+
+	log.SetFlags(log.Lshortfile | log.LstdFlags | log.Ldate)
+    *port=os.Getenv("PORT")
+	ser, err := serve.NewProxyServe(*configPath, *port)
+	if err != nil {
+		fmt.Println("start pproxy failed", err)
+		os.Exit(2)
+	}
+	ser.Debug = *vv
+	ser.Start()
 }
+
