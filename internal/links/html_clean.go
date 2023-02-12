e6c41d4eb7ac7bc8f4d80f7fb663647cb611c553
hidu <duv123@gmail.com>
2023-02-12 17:32:36 +0800

    fix html clean

--- internal/links/html_clean.go
+++ internal/links/html_clean.go
@@ -2,6 +2,7 @@ package links
 
 import (
 	"bytes"
+	"io"
 	"regexp"
 
 	"github.com/tdewolff/minify/v2"
@@ -13,10 +14,16 @@ var m = minify.New()
 
 func init() {
 	m.AddFunc("text/css", css.Minify)
-	// html.DefaultMinifier.KeepQuotes = true
-	// html.DefaultMinifier.KeepEndTags = true
-	// html.DefaultMinifier.KeepWhitespace = true
-	m.AddFunc("text/html", html.Minify)
+	m.AddFunc("text/html", htmlMinify)
+}
+
+func htmlMinify(m *minify.M, w io.Writer, r io.Reader, params map[string]string) error {
+	ins := &html.Minifier{
+		KeepQuotes:     true,
+		KeepEndTags:    true,
+		KeepWhitespace: true,
+	}
+	return ins.Minify(m, w, r, params)
 }
 
 var brReg = regexp.MustCompile(`(?is)(<\s*br\s*/?\s*>\s*?){2,}`)
